464bf868f47da4735890da2a8b6378e76cee576c
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-05-24 15:35:54 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import ReactDOM from "react-dom";
+import React, { Suspense, useEffect, useRef } from "react";
+import { Canvas, Dom, useFrame } from "react-three-fiber";
+import lerp from "lerp";
+import Content from "./components/Content";
+import Plane from "./components/Plane";
+import state from "./store/store";
+import "./styles/styles.css";
+
+function Startup() {
+  const ref = useRef();
+  useFrame(
+    () =>
+      (ref.current.material.opacity = lerp(
+        ref.current.material.opacity,
+        0,
+        0.025
+      ))
+  );
+  return (
+    <Plane
+      ref={ref}
+      color="#0e0e0f"
+      position={[0, 0, 200]}
+      scale={[100, 100, 1]}
+    />
+  );
+}
+
+function App() {
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
+  return (
+    <>
+      <Canvas
+        className="canvas"
+        concurrent
+        pixelRatio={1}
+        orthographic
+        camera={{ zoom: state.zoom, position: [0, 0, 500] }}
+      >
+        <Suspense
+          fallback={<Dom center className="loading" children="Loading..." />}
+        >
+          <Content />
+          <Startup />
+        </Suspense>
+      </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        {new Array(state.sections).fill().map((_, index) => (
+          <div
+            key={index}
+            id={"0" + index}
+            style={{ height: `${(state.pages / state.sections) * 100}vh` }}
+          />
+        ))}
+      </div>
+      <div className="frame">
+        <h1 className="frame__title">MINKS</h1>
+        <div className="frame__links">
+          <a className="frame__link" href="https://github.com/manikmmalhotra/">
+            Github
+          </a>
+          <a
+            className="frame__link"
+            href="https://www.linkedin.com/in/manik-malhotra-029a22193/"
+          >
+            LinkedIn
+          </a>
+          <a className="frame__link resume" href="https://github.com/Luzaks/">
+            Resume
+          </a>
+        </div>
+        <div className="frame__nav">
+          <a className="frame__link" href="#00" children="intro" />
+          <a className="frame__link" href="#01" children="01" />
+          <a className="frame__link" href="#02" children="02" />
+          <a className="frame__link" href="#03" children="03" />
+          <a className="frame__link" href="#04" children="04" />
+          <a className="frame__link" href="#05" children="05" />
+          <a className="frame__link" href="#06" children="06" />
+          <a className="frame__link" href="#07" children="07" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/store/store.js
@@ -0,0 +1,56 @@
+import { createRef } from "react";
+
+const state = {
+  sections: 8,
+  pages: 7.5,
+  zoom: 75,
+  paragraphs: [
+    {
+      offset: 2,
+      factor: 2.0,
+      header: "Tiltzin",
+      image: "/project-2.png",
+      aspect: 1.5,
+      text: "https://www.youtube.com/",
+    },
+    {
+      offset: 3,
+      factor: 2.25,
+      header: "Gamma Gallery",
+      image: "/project-3.png",
+      aspect: 1.5037,
+      text: "A front end gallery application built with React, it fetch Land Rover photos from an external API.",
+    },
+    {
+      offset: 4,
+      factor: 2.0,
+      header: "Facebook Clone",
+      image: "/project-4.png",
+      aspect: 1.5,
+      text: "A clone of the popular social media, this is a full stack application built with Ruby on Rails.",
+    },
+    {
+      offset: 5,
+      factor: 1.75,
+      header: "Dodoist",
+      image: "/project-5.png",
+      aspect: 1.55,
+      text: "A Todo list app built in JavaScript.",
+    },
+    {
+      offset: 6,
+      factor: 1.5,
+      header: "LYNX Shooter Game",
+      image: "/project-1.jpg",
+      aspect: 1.51,
+      text: "A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.A space shooter game built with Phaser 3 framework.",
+    },
+  ],
+  stripes: [
+    { offset: 0, color: "#000", height: 13 },
+    { offset: 6.3, color: "#000", height: 20 },
+  ],
+  top: createRef(),
+};
+
+export default state;
